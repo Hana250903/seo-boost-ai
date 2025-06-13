beb2daa7b32268ebe28e214e755cfdacc57233e5
Hana250903 <kiritomiku0@gmail.com>
2025-06-13 16:02:09 +0700

    fix: update swagger.json

--- SEOBoostAI.API/Program.cs
+++ SEOBoostAI.API/Program.cs
@@ -80,7 +80,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Cursus API v.01");
+    });
 }
 
 app.UseHttpsRedirection();
