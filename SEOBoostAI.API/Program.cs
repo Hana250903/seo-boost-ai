c966c59dd406e145210531be9a3c4f13a76f0c96
Hana250903 <kiritomiku0@gmail.com>
2025-06-13 16:10:41 +0700

    fix: delete evi.Development

--- SEOBoostAI.API/Program.cs
+++ SEOBoostAI.API/Program.cs
@@ -77,14 +77,13 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Cursus API v.01");
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Cursus API v.01");
+});
+
 
 app.UseHttpsRedirection();
 
